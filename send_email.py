73cad4592226af93bf9d029acb3500704aefe1ca
Krishna Kant Hati <krishnakant08@users.noreply.github.com>
2020-11-15 00:17:20 +0530

    new commit

--- send_email.py
+++ send_email.py
@@ -12,24 +12,24 @@ r = sr.Recognizer()
 mic = sr.Microphone()
 
 
-tts = gTTS('What has to be the subject of your eamil')
+tts = gTTS('What has to be the subject of your Eamil')
 tts.save('subject.mp3')
 playsound('subject.mp3')
 with mic as source:
     audio = r.listen(source)
 subject_said = r.recognize_google(audio)
 print(f'You said: {subject_said}')
-tts = gTTS(subject_said)
+tts = gTTS('You said ' + subject_said)
 tts.save('subject_said.mp3')
 playsound('subject_said.mp3')
 
-tts = gTTS('What has to be the body of your eamil')
+tts = gTTS('What has to be the body of your Eamil')
 tts.save('body.mp3')
 playsound('body.mp3')
 with mic as source:
     audio = r.listen(source)
 body_said = r.recognize_google(audio)
-tts = gTTS(body_said)
+tts = gTTS('You said ' + body_said)
 tts.save('body_said.mp3')
 print(f'You said : {body_said}')
 playsound('body_said.mp3')
